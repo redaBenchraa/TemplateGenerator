f005be980a8204516ecf37d10e9c9b699a25424a
Reda Benchraa <reda.benchraa@um5s.net.ma>
2017-06-21 18:16:30 +0000

    @Entity

--- src/main/java/models/Information.java
+++ src/main/java/models/Information.java
@@ -1,12 +1,14 @@
 package models;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 /**
  * Created by reda-benchraa on 21/06/17.
  */
+@Entity
 public class Information {
     @Id
     @Column
