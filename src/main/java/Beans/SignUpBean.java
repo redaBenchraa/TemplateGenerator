e3667407ae4790db5ab652f794683492fd5e1c74
RababChahboune <rabab.chahboune@gmail.com>
2017-06-23 17:50:07 +0000

    signup template

---
+++ src/main/java/Beans/SignUpBean.java
@@ -0,0 +1,12 @@
+package Beans;
+
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
+/**
+ * Created by Rabab Chahboune on 6/22/2017.
+ */
+@ManagedBean
+public class SignUpBean implements Serializable {
+
+}
